708bbb0cd123d74d27ebed4533fb2bc0cbb80f12
Antonella Ortega <antonellaportega@gmail.com>
2023-11-28 13:14:05 -0300

    objeto fecha dia mes año

--- js/app.js
+++ js/app.js
@@ -9,3 +9,12 @@ const parrafoHora = document.getElementById('hora')
 const primerLetraMayuscula = (texto) => {
     return texto[0].toUpperCase() + texto.slice(1)
 }
+
+// objeto fecha (dia-mes-año)
+const fecha = new Date()
+let nombreDia = fecha.toLocaleString('es', {weekday: 'long'})
+let diaMes = fecha.getDate()
+let mes = fecha.toLocaleString('es', { month: 'long'});
+let anio = fecha.toLocaleString('es', { year: 'numeric'});
+let fechaCompleta = `${primerLetraMayuscula(nombreDia)} ${diaMes} de ${primerLetraMayuscula(mes)} del ${anio}`
+dia.innerText = fechaCompleta
