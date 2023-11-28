10ed0a3686490b76bbce2ebc14599b2b2dc0221e
Antonella Ortega <antonellaportega@gmail.com>
2023-11-28 13:12:38 -0300

    funcion primer letra mayusucula

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@ const reloj = document.getElementsById('reloj')
 const dia = document.getElementById('dia')
 const parrafoHora = document.getElementById('hora')
 
+// funciones
+
+const primerLetraMayuscula = (texto) => {
+    return texto[0].toUpperCase() + texto.slice(1)
+}
